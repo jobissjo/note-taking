1d432c69ccc88a645a65d717c811d430e9a233b8
Jobi S S <jobisjobi1234@gmail.com>
2026-03-23 22:33:13 +0530

    port issue trinng to fix

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 5000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
